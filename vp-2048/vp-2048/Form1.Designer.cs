fbb6e540a39909f3677468befc8f8aaf14991963
Ivan <94326614+iivanov3@users.noreply.github.com>
2022-06-30 17:55:57 +0200

    fixed rendering

--- vp-2048/vp-2048/Form1.Designer.cs
+++ vp-2048/vp-2048/Form1.Designer.cs
@@ -40,17 +40,18 @@ namespace vp_2048
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(604, 202);
+            this.label1.Location = new System.Drawing.Point(690, 269);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(604, 53);
+            this.button1.Location = new System.Drawing.Point(690, 71);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(135, 23);
+            this.button1.Size = new System.Drawing.Size(154, 31);
             this.button1.TabIndex = 1;
             this.button1.Text = "handleMoveRight";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,9 +59,10 @@ namespace vp_2048
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(604, 82);
+            this.button2.Location = new System.Drawing.Point(690, 109);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(135, 23);
+            this.button2.Size = new System.Drawing.Size(154, 31);
             this.button2.TabIndex = 2;
             this.button2.Text = "handleMoveTop";
             this.button2.UseVisualStyleBackColor = true;
@@ -68,9 +70,10 @@ namespace vp_2048
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(604, 111);
+            this.button3.Location = new System.Drawing.Point(690, 148);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(135, 23);
+            this.button3.Size = new System.Drawing.Size(154, 31);
             this.button3.TabIndex = 3;
             this.button3.Text = "handleMoveLeft";
             this.button3.UseVisualStyleBackColor = true;
@@ -78,9 +81,10 @@ namespace vp_2048
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(604, 140);
+            this.button4.Location = new System.Drawing.Point(690, 187);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(135, 23);
+            this.button4.Size = new System.Drawing.Size(154, 31);
             this.button4.TabIndex = 4;
             this.button4.Text = "handleMoveBottom";
             this.button4.UseVisualStyleBackColor = true;
@@ -89,26 +93,28 @@ namespace vp_2048
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(602, 21);
+            this.label2.Location = new System.Drawing.Point(688, 28);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
+            this.label2.Size = new System.Drawing.Size(50, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "label2";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- vp-2048/vp-2048/Form1.cs
+++ vp-2048/vp-2048/Form1.cs
@@ -19,111 +19,64 @@ namespace vp_2048
         public Form1()
         {
             InitializeComponent();
-            
+            DoubleBuffered = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             game = new Game();
-
             this.updateTable();
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
-            if (game.isLost || game.isWon) 
+            if (game.isLost || game.isWon)
+            {
                 return base.ProcessCmdKey(ref msg, keyData);
-            
+            }
+            bool changed = false;
             if (keyData == Keys.Left)
             {
                 game.handleMove("left");
-                this.updateTable();
-                if (this.game.isWon)
-                    MessageBox.Show("Won");
-                else if (this.game.isLost)
-                    MessageBox.Show("Lost");
-                return true;
+                changed = true;
             }
-            if (keyData == Keys.Right)
+            else if (keyData == Keys.Right)
             {
                 game.handleMove("right");
-                this.updateTable();
-                if (this.game.isWon)
-                    MessageBox.Show("Won");
-                else if (this.game.isLost)
-                    MessageBox.Show("Lost");
-
-                return true;
+                changed = true;
             }
-            if (keyData == Keys.Up)
+            else if (keyData == Keys.Up)
             {
                 game.handleMove("top");
-                this.updateTable();
-                if (this.game.isWon)
-                    MessageBox.Show("Won");
-                else if (this.game.isLost)
-                    MessageBox.Show("Lost");
-
-                return true;
+                changed = true;
             }
-            if (keyData == Keys.Down)
+            else if (keyData == Keys.Down)
             {
                 game.handleMove("bottom");
+                changed = true;
+            }
+            
+            if (changed)
+            {
                 this.updateTable();
                 if (this.game.isWon)
-                    MessageBox.Show("Won");
-                else if (this.game.isLost)
-                    MessageBox.Show("Lost");
-
-                return true;
-            }
-           
-
-            return base.ProcessCmdKey(ref msg, keyData);
-        }
-        protected override void OnPaint(PaintEventArgs e)
-        {
-            base.OnPaint(e);
-            Pen myBrush = new Pen(Color.Yellow);
-            SolidBrush solidBrush = new SolidBrush(Color.Orange);
-
-
-            SolidBrush drawBrush = new SolidBrush(Color.Black);
-
-
-            // Set format of string.
-            StringFormat drawFormat = new StringFormat();
-            drawFormat.Alignment = StringAlignment.Center;
-            drawFormat.LineAlignment = StringAlignment.Center;
-
-
-            formGraphics = this.CreateGraphics();
-
-            for (int i = 0; i < 4; i++)
-            {
-                for (int j = 0; j < 4; j++)
                 {
-                    String drawString = game.getTileValue(j, i).ToString();
-                    Rectangle r = new Rectangle(10 + 101 * i, 10 + 101 * j, 100, 100);
-               
-                    formGraphics.DrawRectangle(myBrush, r);
-                    formGraphics.FillRectangle(solidBrush, r);
-                    formGraphics.DrawString(drawString, drawFont, drawBrush, r, drawFormat);
+                    MessageBox.Show("Won");
+                }
+                else if (this.game.isLost)
+                {
+                    MessageBox.Show("Lost");
                 }
             }
 
-
-            myBrush.Dispose();
-            formGraphics.Dispose();
-            solidBrush.Dispose();
-            drawBrush.Dispose();
-
+            return base.ProcessCmdKey(ref msg, keyData);
         }
-    
+        
         private void updateTable()
         {
             int[,] arr = new int[4, 4];
             arr = game.getTable();
+            
             string arrTSr = "";
             for (int i = 0; i < 4; i++)
             {
@@ -133,19 +86,15 @@ namespace vp_2048
                 }
                 arrTSr += "\n";
             }
+
             label1.Text = arrTSr;
-
-            this.Invalidate();
-
             label2.Text = "Score: " + this.game.getScore();
-           
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             game.handleMove("right");
             this.updateTable();
-
         }
         private void button2_Click(object sender, EventArgs e)
         {
@@ -162,6 +111,37 @@ namespace vp_2048
         {
             game.handleMove("bottom");
             this.updateTable();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen myBrush = new Pen(Color.Yellow);
+            SolidBrush solidBrush = new SolidBrush(Color.Orange);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            // Set format of string.
+            StringFormat drawFormat = new StringFormat();
+            drawFormat.Alignment = StringAlignment.Center;
+            drawFormat.LineAlignment = StringAlignment.Center;
+
+            formGraphics = this.CreateGraphics();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    String drawString = game.getTileValue(j, i).ToString();
+                    Rectangle r = new Rectangle(10 + 101 * i, 10 + 101 * j, 100, 100);
+
+                    formGraphics.DrawRectangle(myBrush, r);
+                    formGraphics.FillRectangle(solidBrush, r);
+                    formGraphics.DrawString(drawString, drawFont, drawBrush, r, drawFormat);
+                }
+            }
+
+            myBrush.Dispose();
+            formGraphics.Dispose();
+            solidBrush.Dispose();
+            drawBrush.Dispose();
         }
     }
 }
