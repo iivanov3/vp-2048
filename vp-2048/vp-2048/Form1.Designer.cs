f7528c5baafe79a6a7ff29e68e1aa2e1e22f6ed5
Ivan <94326614+iivanov3@users.noreply.github.com>
2022-07-01 20:03:26 +0200

    added tile colors, tiles no longer display zeros

--- vp-2048/vp-2048/Form1.Designer.cs
+++ vp-2048/vp-2048/Form1.Designer.cs
@@ -29,24 +29,14 @@ namespace vp_2048
         /// </summary>
         private void InitializeComponent()
         {
-            this.label2 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(688, 28);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
-            this.label2.Paint += new System.Windows.Forms.PaintEventHandler(this.label2_Paint);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(690, 74);
+            this.button2.Location = new System.Drawing.Point(510, 73);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(154, 29);
             this.button2.TabIndex = 6;
@@ -56,7 +46,7 @@ namespace vp_2048
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(690, 130);
+            this.button1.Location = new System.Drawing.Point(510, 120);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(154, 29);
             this.button1.TabIndex = 7;
@@ -64,11 +54,21 @@ namespace vp_2048
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(508, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Paint += new System.Windows.Forms.PaintEventHandler(this.label2_Paint);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 600);
+            this.ClientSize = new System.Drawing.Size(678, 468);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
@@ -83,9 +83,9 @@ namespace vp_2048
         }
 
         #endregion
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- vp-2048/vp-2048/Form1.cs
+++ vp-2048/vp-2048/Form1.cs
@@ -16,18 +16,40 @@ namespace vp_2048
         private Font drawFont = new Font("Arial", 25);
         private Graphics formGraphics;
         private Rules rules;
+        private readonly Dictionary<int, Color> tileColors;
+        private readonly Dictionary<Keys, string> validKeys;
 
         public Form1()
         {
             InitializeComponent();
             DoubleBuffered = true;
             rules = new Rules();
+            label2.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+
+            tileColors = new Dictionary<int, Color>();
+            tileColors[0] = Color.FromArgb(204, 192, 179);
+            tileColors[2] = Color.FromArgb(238, 228, 218);
+            tileColors[4] = Color.FromArgb(237, 224, 200);
+            tileColors[8] = Color.FromArgb(242, 177, 121);
+            tileColors[16] = Color.FromArgb(245, 149, 99);
+            tileColors[32] = Color.FromArgb(246, 124, 95);
+            tileColors[64] = Color.FromArgb(246, 94, 59);
+            tileColors[128] = Color.FromArgb(237, 207, 114);
+            tileColors[256] = Color.FromArgb(237, 204, 97);
+            tileColors[512] = Color.FromArgb(237, 200, 80);
+            tileColors[1024] = Color.FromArgb(237, 197, 63);
+            tileColors[2048] = Color.FromArgb(237, 194, 46);
+
+            validKeys = new Dictionary<Keys, string>();
+            validKeys[Keys.Up] = "top";
+            validKeys[Keys.Down] = "bottom";
+            validKeys[Keys.Left] = "left";
+            validKeys[Keys.Right] = "right";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             game = new Game();
-            label2.Font = new Font("Segoe UI", 14, FontStyle.Bold);
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -36,30 +58,11 @@ namespace vp_2048
             {
                 return base.ProcessCmdKey(ref msg, keyData);
             }
-            bool changed = false;
-            if (keyData == Keys.Left)
+
+            if (validKeys.ContainsKey(keyData))
             {
-                game.handleMove("left");
-                changed = true;
-            }
-            else if (keyData == Keys.Right)
-            {
-                game.handleMove("right");
-                changed = true;
-            }
-            else if (keyData == Keys.Up)
-            {
-                game.handleMove("top");
-                changed = true;
-            }
-            else if (keyData == Keys.Down)
-            {
-                game.handleMove("bottom");
-                changed = true;
-            }
-            
-            if (changed)
-            {
+                game.handleMove(validKeys[keyData]);
+
                 if (this.game.isWon)
                 {
                     MessageBox.Show(String.Format("You win.\nYour score is {0}.", game.getScore()), "Game over");
@@ -76,7 +79,6 @@ namespace vp_2048
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Pen myBrush = new Pen(Color.Yellow);
-            SolidBrush solidBrush = new SolidBrush(Color.Orange);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
 
             // Set format of string.
@@ -85,16 +87,23 @@ namespace vp_2048
             drawFormat.LineAlignment = StringAlignment.Center;
 
             formGraphics = this.CreateGraphics();
+
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
+                    int val = game.getTileValue(j, i);
+                    SolidBrush solidBrush = new SolidBrush(tileColors[val]);
                     String drawString = game.getTileValue(j, i).ToString();
                     Rectangle r = new Rectangle(10 + 101 * i, 10 + 101 * j, 100, 100);
 
                     formGraphics.DrawRectangle(myBrush, r);
                     formGraphics.FillRectangle(solidBrush, r);
-                    formGraphics.DrawString(drawString, drawFont, drawBrush, r, drawFormat);
+                    if (val != 0)
+                    {
+                        formGraphics.DrawString(drawString, drawFont, drawBrush, r, drawFormat);
+                    }
+                    solidBrush.Dispose();
                 }
             }
 
@@ -102,14 +111,13 @@ namespace vp_2048
 
             myBrush.Dispose();
             formGraphics.Dispose();
-            solidBrush.Dispose();
             drawBrush.Dispose();
         }
 
         private void button2_Click_1(object sender, EventArgs e)
         {
             game = new Game();
-            Invalidate();
+            Invalidate(true);
         }
 
         private void button1_Click_1(object sender, EventArgs e)
